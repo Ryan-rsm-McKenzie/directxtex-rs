32f4384ea412994f4b4d7cc85aa16f7e252827a4
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2024-01-22 02:46:31 -0800

    redo how ffi builds are handled

--- build.rs
+++ build.rs
@@ -3,39 +3,38 @@
 use cc::Build;
 use std::path::Path;
 
+fn make_standard_build() -> Build {
+    let mut build = Build::new();
+    build
+        .cpp(true)
+        .std("c++17")
+        .warnings(false)
+        .extra_warnings(false)
+        .includes([
+            "external/DirectX-Headers/include",
+            "external/DirectXMath/Inc",
+            "external/DirectXTex/DirectXTex",
+        ]);
+    if !cfg!(windows) {
+        build.includes(["external/DirectX-Headers/include/wsl/stubs", "ffi/include"]);
+    }
+    build
+}
+
 fn build_headers() {
     let root = Path::new("external/DirectX-Headers");
-    let mut headers = Build::new();
-    let mut guids = Build::new();
+    let mut headers = make_standard_build();
+    let mut guids = make_standard_build();
     let tool = headers.get_compiler();
 
     headers
-        .cpp(true)
-        .std("c++14")
-        .warnings(false)
-        .extra_warnings(false)
-        .includes(
-            ["include", "include/directx"]
-                .into_iter()
-                .map(|x| root.join(x)),
-        )
+        .include(root.join("include/directx"))
         .file(root.join("src/d3dx12_property_format_table.cpp"));
     guids
-        .cpp(true)
-        .std("c++14")
-        .warnings(false)
-        .extra_warnings(false)
-        .includes(
-            ["include", "include/directx"]
-                .into_iter()
-                .map(|x| root.join(x)),
-        )
+        .include(root.join("include/directx"))
         .file(root.join("src/dxguids.cpp"));
 
-    if !cfg!(windows) {
-        headers.include(root.join("include/wsl/stubs"));
-        guids.include(root.join("include/wsl/stubs"));
-    } else if tool.is_like_gnu() || tool.is_like_clang() {
+    if !cfg!(windows) && (tool.is_like_gnu() || tool.is_like_clang()) {
         headers.define("__REQUIRED_RPCNDR_H_VERSION__", "475");
         guids.define("__REQUIRED_RPCNDR_H_VERSION__", "475");
     }
@@ -46,39 +45,29 @@ fn build_headers() {
 
 fn build_tex() {
     let root = Path::new("external/DirectXTex");
-    let mut build = Build::new();
+    let mut build = make_standard_build();
 
-    build
-        .cpp(true)
-        .std("c++17")
-        .warnings(false)
-        .extra_warnings(false)
-        .files(
-            [
-                "DirectXTex/BC.cpp",
-                "DirectXTex/BC4BC5.cpp",
-                "DirectXTex/BC6HBC7.cpp",
-                "DirectXTex/DirectXTexCompress.cpp",
-                "DirectXTex/DirectXTexConvert.cpp",
-                "DirectXTex/DirectXTexDDS.cpp",
-                "DirectXTex/DirectXTexHDR.cpp",
-                "DirectXTex/DirectXTexImage.cpp",
-                "DirectXTex/DirectXTexMipmaps.cpp",
-                "DirectXTex/DirectXTexMisc.cpp",
-                "DirectXTex/DirectXTexNormalMaps.cpp",
-                "DirectXTex/DirectXTexPMAlpha.cpp",
-                "DirectXTex/DirectXTexResize.cpp",
-                "DirectXTex/DirectXTexTGA.cpp",
-                "DirectXTex/DirectXTexUtil.cpp",
-            ]
-            .into_iter()
-            .map(|x| root.join(x)),
-        )
-        .include(root.join("DirectXTex"))
-        .includes([
-            "external/DirectXMath/Inc",
-            "external/DirectX-Headers/include",
-        ]);
+    build.files(
+        [
+            "DirectXTex/BC.cpp",
+            "DirectXTex/BC4BC5.cpp",
+            "DirectXTex/BC6HBC7.cpp",
+            "DirectXTex/DirectXTexCompress.cpp",
+            "DirectXTex/DirectXTexConvert.cpp",
+            "DirectXTex/DirectXTexDDS.cpp",
+            "DirectXTex/DirectXTexHDR.cpp",
+            "DirectXTex/DirectXTexImage.cpp",
+            "DirectXTex/DirectXTexMipmaps.cpp",
+            "DirectXTex/DirectXTexMisc.cpp",
+            "DirectXTex/DirectXTexNormalMaps.cpp",
+            "DirectXTex/DirectXTexPMAlpha.cpp",
+            "DirectXTex/DirectXTexResize.cpp",
+            "DirectXTex/DirectXTexTGA.cpp",
+            "DirectXTex/DirectXTexUtil.cpp",
+        ]
+        .into_iter()
+        .map(|x| root.join(x)),
+    );
 
     if cfg!(windows) {
         build
@@ -91,8 +80,6 @@ fn build_tex() {
                 .map(|x| root.join(x)),
             )
             .object("Ole32.lib");
-    } else {
-        build.includes(["external/DirectX-Headers/include/wsl/stubs", "ffi/include"]);
     }
 
     build.compile("DirectXTex");
@@ -100,20 +87,10 @@ fn build_tex() {
 
 fn build_ffi() {
     let root = Path::new("ffi");
-    let mut build = Build::new();
-    build
-        .cpp(true)
-        .std("c++17")
-        .warnings(false)
-        .extra_warnings(false)
+    make_standard_build()
         .file(root.join("main.cpp"))
         .include(root)
-        .includes([
-            "external/DirectX-Headers/Include",
-            "external/DirectXMath/Inc",
-            "external/DirectXTex/DirectXTex",
-        ]);
-    build.compile("directxtex-ffi");
+        .compile("directxtex-ffi");
 }
 
 fn main() {
