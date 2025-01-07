29b19ee0581e6afc5b52662f5a0c9b9f028454b3
Ryan McKenzie <ryan__mckenzie@hotmail.com>
2025-01-07 11:52:21 -0800

    tabs -> spaces

--- src/macros.rs
+++ src/macros.rs
@@ -1,142 +1,142 @@
 macro_rules! c_enum {
-	(
-		$(#[doc=$enumeration_doc:literal])*
-		$module:ident::$enumeration:ident($underlying:ty) => {$(
-			$(#[doc=$variant_doc:literal])*
-			$variant:ident = $value:literal,
-		)*}
+    (
+        $(#[doc=$enumeration_doc:literal])*
+        $module:ident::$enumeration:ident($underlying:ty) => {$(
+            $(#[doc=$variant_doc:literal])*
+            $variant:ident = $value:literal,
+        )*}
 
-		$(extra => {$(
-			$(#[doc=$extra_variant_doc:literal])*
-			$extra_variant:ident = $extra_value:literal,
-		)*})?
-	) => {
-		#[allow(non_camel_case_types)]
-		#[derive(Clone, Copy, Eq, Hash, PartialEq, PartialOrd, Ord)]
-		#[repr(transparent)]
-		$(#[doc = $enumeration_doc])*
-		pub struct $enumeration($underlying);
+        $(extra => {$(
+            $(#[doc=$extra_variant_doc:literal])*
+            $extra_variant:ident = $extra_value:literal,
+        )*})?
+    ) => {
+        #[allow(non_camel_case_types)]
+        #[derive(Clone, Copy, Eq, Hash, PartialEq, PartialOrd, Ord)]
+        #[repr(transparent)]
+        $(#[doc = $enumeration_doc])*
+        pub struct $enumeration($underlying);
 
-		impl $enumeration {
-			$(
-				$(#[doc = $variant_doc])*
-				pub const $variant: Self = Self($value);
-			)*
+        impl $enumeration {
+            $(
+                $(#[doc = $variant_doc])*
+                pub const $variant: Self = Self($value);
+            )*
 
-			$($(
-				$(#[doc = $extra_variant_doc])*
-				pub const $extra_variant: Self = Self($extra_value);
-			)*)?
+            $($(
+                $(#[doc = $extra_variant_doc])*
+                pub const $extra_variant: Self = Self($extra_value);
+            )*)?
 
-			#[must_use]
-			pub const fn bits(self) -> $underlying {
-				self.0
-			}
-		}
+            #[must_use]
+            pub const fn bits(self) -> $underlying {
+                self.0
+            }
+        }
 
-		impl Default for $enumeration {
-			fn default() -> Self {
-				Self(0)
-			}
-		}
+        impl Default for $enumeration {
+            fn default() -> Self {
+                Self(0)
+            }
+        }
 
-		impl From<$enumeration> for $underlying {
-			fn from(value: $enumeration) -> Self {
-				value.0
-			}
-		}
+        impl From<$enumeration> for $underlying {
+            fn from(value: $enumeration) -> Self {
+                value.0
+            }
+        }
 
-		impl From<$underlying> for $enumeration {
-			fn from(value: $underlying) -> Self {
-				Self(value)
-			}
-		}
+        impl From<$underlying> for $enumeration {
+            fn from(value: $underlying) -> Self {
+                Self(value)
+            }
+        }
 
-		impl core::fmt::Debug for $enumeration {
-			fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-				match self.0 {
-					$($value => write!(f, "{}", core::stringify!($variant)),)*
-					$($(
-						#[allow(unreachable_patterns)]
-						$extra_value => write!(f, "{}", core::stringify!($extra_variant)),
-					)*)?
-					unk => write!(f, "0x{unk:x}"),
-				}
-			}
-		}
+        impl core::fmt::Debug for $enumeration {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                match self.0 {
+                    $($value => write!(f, "{}", core::stringify!($variant)),)*
+                    $($(
+                        #[allow(unreachable_patterns)]
+                        $extra_value => write!(f, "{}", core::stringify!($extra_variant)),
+                    )*)?
+                    unk => write!(f, "0x{unk:x}"),
+                }
+            }
+        }
 
-		pub(crate) mod $module {
-			use super::$enumeration;
+        pub(crate) mod $module {
+            use super::$enumeration;
 
-			$(
-				$(#[doc = $variant_doc])*
-				pub const $variant: $enumeration = $enumeration($value);
-			)*
+            $(
+                $(#[doc = $variant_doc])*
+                pub const $variant: $enumeration = $enumeration($value);
+            )*
 
-			$($(
-				$(#[doc = $extra_variant_doc])*
-				pub const $extra_variant: $enumeration = $enumeration($extra_value);
-			)*)?
-		}
-	};
+            $($(
+                $(#[doc = $extra_variant_doc])*
+                pub const $extra_variant: $enumeration = $enumeration($extra_value);
+            )*)?
+        }
+    };
 }
 
 pub(crate) use c_enum;
 
 macro_rules! c_bits {
-	(
-		$(#[doc=$enumeration_doc:literal])*
-		$module:ident::$enumeration:ident($underlying:ty) => {$(
-			$(#[doc=$variant_doc:literal])*
-			$variant:ident = $value:literal,
-		)*}
+    (
+        $(#[doc=$enumeration_doc:literal])*
+        $module:ident::$enumeration:ident($underlying:ty) => {$(
+            $(#[doc=$variant_doc:literal])*
+            $variant:ident = $value:literal,
+        )*}
 
-		$(extra => {$(
-			$(#[doc=$extra_variant_doc:literal])*
-			$extra_variant:ident = $extra_value:expr;
-		)*})?
-	) => {
-		bitflags::bitflags! {
-			#[allow(non_camel_case_types)]
-			#[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
-			#[repr(transparent)]
-			$(#[doc = $enumeration_doc])*
-			pub struct $enumeration: $underlying {
-				$(
-					$(#[doc = $variant_doc])*
-					const $variant = $value;
-				)*
-				const _ = !0;
-			}
-		}
+        $(extra => {$(
+            $(#[doc=$extra_variant_doc:literal])*
+            $extra_variant:ident = $extra_value:expr;
+        )*})?
+    ) => {
+        bitflags::bitflags! {
+            #[allow(non_camel_case_types)]
+            #[derive(Clone, Copy, Debug, Default, Eq, Hash, PartialEq)]
+            #[repr(transparent)]
+            $(#[doc = $enumeration_doc])*
+            pub struct $enumeration: $underlying {
+                $(
+                    $(#[doc = $variant_doc])*
+                    const $variant = $value;
+                )*
+                const _ = !0;
+            }
+        }
 
-		impl $enumeration {
-			$($(
-				$(#[doc = $extra_variant_doc])*
-				pub const $extra_variant: Self = $extra_value;
-			)*)?
+        impl $enumeration {
+            $($(
+                $(#[doc = $extra_variant_doc])*
+                pub const $extra_variant: Self = $extra_value;
+            )*)?
 
-			#[allow(unused)]
-			#[must_use]
-			pub(crate) fn bits_fixed(self) -> u32 {
-				self.bits() as u32
-			}
-		}
+            #[allow(unused)]
+            #[must_use]
+            pub(crate) fn bits_fixed(self) -> u32 {
+                self.bits() as u32
+            }
+        }
 
-		pub(crate) mod $module {
-			use super::$enumeration;
+        pub(crate) mod $module {
+            use super::$enumeration;
 
-			$(
-				$(#[doc = $variant_doc])*
-				pub const $variant: $enumeration = unsafe { $enumeration::from_bits($value).unwrap_unchecked() };
-			)*
+            $(
+                $(#[doc = $variant_doc])*
+                pub const $variant: $enumeration = unsafe { $enumeration::from_bits($value).unwrap_unchecked() };
+            )*
 
-			$($(
-				$(#[doc = $extra_variant_doc])*
-				pub const $extra_variant: $enumeration = $extra_value;
-			)*)?
-		}
-	};
+            $($(
+                $(#[doc = $extra_variant_doc])*
+                pub const $extra_variant: $enumeration = $extra_value;
+            )*)?
+        }
+    };
 }
 
 pub(crate) use c_bits;
